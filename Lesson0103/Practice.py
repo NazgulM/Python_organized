db564c7778ca485f1a9147a43eed1cfcf8e1fefa
NazgulM <92852651+NazgulM@users.noreply.github.com>
2022-03-01 16:56:39 +0100

    Add files via upload

---
+++ Lesson0103/Practice.py
@@ -0,0 +1,86 @@
+# for i(peremennaya) in <object>:
+# for i in range(4):
+#     print('inside', i)
+#     i = 'hello'
+#     print('outside', i)
+
+# for i in range(100,1000):
+#     if i %2 == 0 and i % 7 ==0: # divide to 2 and 7
+#         print(i)
+
+
+# s = 0
+# for i in range(1,6):
+#     s= s+i
+#     print(s)
+#
+
+# pr = 1
+# for i in range(1,4):
+#     pr = pr * i
+#     print(pr)
+
+# for i in range(4):
+#     print('hello')
+#
+
+# n = int(input('Enter the number: '))
+# for i in range(n):
+#     print('hello')
+
+# from random import randint
+# s = 0
+# for i in range(5):
+#     a  = randint(1,10)
+#     s += a
+#     print(a, end=' ')
+#
+#     print(s,end=' ')
+
+# for i in range(1,11):
+# #     print('*' * i)
+# for i in range(1,11):
+#     print(2**i)
+
+# n = int(input("Enter a number:"))
+# s = 0
+# for i in range(n):
+#     a = int(input())
+#     s += a
+#     print('Current s: ', s)
+#     print('TotalS: ',s)
+
+# print()
+# a = [43,65,3,54,6]
+# count = 0
+# for i in a:
+#     i +=5
+#     print(i, end=' ')
+# print('\n',a)
+# print()
+
+# for i in a:
+#     print(i, a.index(i))
+# n = len(a)
+# for i in range(4):
+#     print(i, a[i])
+#     a[i] +=5
+#     print(a)
+
+# a = [1,2,3,4,32,4,5,3,5]
+# #
+# even = []
+# odd = []
+# n = len(a)
+# for i in range(n):
+#     if a[i] % 2 ==0:
+#         even.append(i)
+#     else:
+#         odd.append(i+1)
+#         print(even)
+#         print(odd)
+
+
+# s[1] --> s[2]
+# s[3] --> s[4]
+# s[i]--> s[i+1]
