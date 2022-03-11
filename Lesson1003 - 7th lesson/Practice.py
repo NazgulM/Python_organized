38091f72dc3fd78dce1de032e7cff02597bf2048
NazgulM <maniyasova.n@gmail.com>
2022-03-11 10:02:17 +0100

    Practice

--- Lesson1003 - 7th lesson/Practice.py
+++ Lesson1003 - 7th lesson/Practice.py
@@ -1,6 +1,17 @@
 # Task 1
+# Create a program that user input length, key and value of dictionary
+n = int(input('Number of elements'))
+d = {}
+
+for i in range(n):
+    key = input('Key: ')
+    value = input('Value')
+    d[key] = value
+
+print(d)
 
 # Task 2
+print()
 numbers = {'One': 1, 'Two': 2, 'Three': 3, 'Twenty': 20, 'Fifty': 50}
 numbers.popitem()
 print(numbers)
@@ -31,6 +42,18 @@ for number in range(1,8):
     dicts[number] = number*number
     print(dicts)
 
+# Task 5
+d = {'One': 'Python', 'Two': 'C++', 'Three': 'Java', 'Four': 'C#'}
+# d2 = dict(d)
+d2 = d.copy()
+print()
+print(d2)
+
+d2.pop('Three')
+print(d2)
+
+d2['New'] = 'Kotlin'
+print(d2)
 
 
 
