966073960a99f9ccafcbf3b9111ee1e13bc0d885
NazgulM <maniyasova.n@gmail.com>
2022-04-27 23:24:05 +0200

    Practice OOP

--- Lesson 20 - OOP Inheritance/repeatOOP.py
+++ Lesson 20 - OOP Inheritance/repeatOOP.py
@@ -138,21 +138,75 @@ class Soda:
 
 
 class TriangleChecker():
-    def __init__(self, num=[]):
+    def __init__(self, num):
         self.num = num
 
     def is_triangle(self):
-        for n in self.num:
-            if n >= 0:
-                print('Woohoo, we can create a triangle')
-                break
-            elif n < 0:
-                print('Nothing will work with negative numbers!')
-                break
-        # elif type(self.num) == str:
-        #     print('You only need to enter numbers!')
-        # else:
-        #     print('Unfortunately, but you cannot make a triangle out of this')
+
+        if len(self.num) >= 0:
+            print('Woohoo, we can create a triangle')
+        elif len(self.num) < 0:
+            print('Nothing will work with negative numbers!')
+        elif type(self.num) == str:
+            print('You only need to enter numbers!')
+        else:
+            print('Unfortunately, but you cannot make a triangle out of this')
+
+
+"""
+Есть три класса Donkey, Horse, Mul
+• Класс Donkey имеет атрибуты name, age, living_area, typeDonkey, метод display( )
+“Осел по имени Spirit живет [место обитания] и он относится к грузовым ослам,
+ ему [возраст] лет”
+• Класс Horse имеет атрибуты name, age, typeHorse, breed метод horce_race( )
+ где есть следующая реализация:
+“Лошадь с именем [имя] и с породой [порода] участвует в забеге ей [возраст] лет”
+• Класс Mul это смесь лошади с ослом (множественное наследование). 
+Также помимо атрибутов, которые были взяты от прародителей, 
+есть свой метод в котором происходит объединение двух методов в один метод 
+с родительских классов: display( ), race( ), но также классе Mul 
+необходимо создать метод eating( ), где есть реализация:
+«Мул может кушать много травы»
+"""
+
+
+class Donkey:
+    def __init__(self, name, age, living_area, typeDonkey):
+        self.nameDonkey = name
+        self.ageDonkey = age
+        self.livingAreaD = living_area
+        self.typeDonkey = typeDonkey
+
+    def display(self):
+        print(f'The Donkey named {self.nameDonkey} lives in {self.livingAreaD} and he belongs to {self.typeDonkey}, '
+              f'he is {self.ageDonkey} years')
+
+
+class Horse:
+    def __init__(self, name, age, typeHorse, breed):
+        self.nameHorse = name
+        self.ageHorse = age
+        self.typeHorse = typeHorse
+        self.breed = breed
+
+    def horse_race(self):
+        print(f'Horse with name {self.nameHorse} and with the breed {self.breed}'
+              f' participates in the race at {self.ageHorse} years')
+
+
+class Mul(Donkey, Horse):
+    def __init__(self,name,age,living_area,typeDonkey, typeHorse, breed):
+        Donkey.__init__(self,name,age,living_area,typeDonkey)
+        Horse.__init__(self,name,age,typeHorse, breed)
+
+    def display(self):
+        super().display()
+        super().horse_race()
+
+    def eating(self):
+        print('Mule can eat a lot of grass')
+
+
 
 
 def main():
@@ -226,14 +280,28 @@ def main():
     drink3.show_my_drink()
     print('**********************************')
 
-    triangle1 = TriangleChecker([2,3,4])
+    triangle1 = TriangleChecker([2, 3, 4])
     triangle1.is_triangle()
-    # triangle2 = TriangleChecker([77, 3, 4])
-    # triangle2.is_triangle()
-    # triangle3 = TriangleChecker([77, 3, 'Side 3'])
-    # triangle3.is_triangle()
+    triangle2 = TriangleChecker([77, 3, 4])
+    triangle2.is_triangle()
+    triangle3 = TriangleChecker([77, 3, 'Side 3'])
+    triangle3.is_triangle()
     triangle4 = TriangleChecker([77, -3, 4])
     triangle4.is_triangle()
+    print('*************************************')
+
+    donkey1 = Donkey('Spirit', 5, 'Somalia', 'cargo donkeys')
+    donkey1.display()
+    print('*************************************')
+
+    horse1 = Horse('Jackie', 4, 'racing horse', 'Arabian')
+    horse1.horse_race()
+    print('*************************************')
+
+    mul1 = Mul('Jerry', 15, 'Central America', 'Hinny','female', 'Arabian')
+    mul1.display()
+    mul1.eating()
+
 
 
 if __name__ == '__main__':
