7f3977786efd727099fc86229da3fba78b188897
NazgulM <92852651+NazgulM@users.noreply.github.com>
2022-08-23 09:11:21 +0200

    Add files via upload

---
+++ Lesson2_WritingExcel/Lesson2_parsingTable.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+def get_static_league(url):
+    result = requests.get(url)
+    soup = bs(result.text, 'html.parser')
+
+    table_list = []
+    league_table = soup.find('table',class_='standing-table__table')
+    # print(league_table.text)
+
+    for data in league_table.find_all('tbody'):
+        rows =  data.find_all('tr', class_='standing-table__row')
+        for row in rows:
+            team_name = row.find('td',class_='standing-table__cell standing-table__cell--name').text.strip()
+            pl_points = row.find_all('td', class_='standing-table__cell')[9].text
+
+            teampPremLegData =  {
+                'name_team': team_name,
+                'points': pl_points
+            }
+
+            table_list.append(teampPremLegData)
+
+        return table_list
+
+
+def main():
+    url = 'https://www.skysports.com/premier-league-table'
+    data = get_static_league(url)
+
+    df = pd.DataFrame(data)
+    # print(df)
+
+    df.to_excel('EnglandTeamPts.xlsx')
+
+
+
+if __name__ == '__main__':
+    main()
+
+
